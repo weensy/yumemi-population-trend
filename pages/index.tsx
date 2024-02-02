6832bf007722d2307fedc228b83c05f5d5ec93e5
weensy <hello@weensy.dev>
2024-02-02 09:03:26 +0900

    Add meta in head

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,6 +66,9 @@ export default function Home() {
     <>
       <Head>
         <title>総人口推移</title>
+        <meta name="description" content="都道府県別総人口推移グラフ" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <div>
